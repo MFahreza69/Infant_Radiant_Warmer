49a48ab97b90f4f535dfab4b849f1c43b75cfa30
MFahreza69 <110213902+MFahreza69@users.noreply.github.com>
2023-01-04 15:08:55 +0700

    Mayor change at-tiny1616
    - adding average value for filtering
    - adding function for using 2 type of sensor/probe skin
    - changing communication data with atmega64 from 10bit to 20 bit data

--- atTiny_SkinTemp/atTiny_SkinTemp.ino
+++ atTiny_SkinTemp/atTiny_SkinTemp.ino
@@ -1,29 +1,40 @@
 #define dataOut 4 //PB5
 #define clkIn 5  //PB4
-#define highSensor 16 //PA3
-#define lowSensor 0 //PA4
+#define highSensor 16 //2
+#define lowSensor 0 //3
 
-//test
+
+
 int a;
+int a2;
 int dat;
 int dat2;
 unsigned long low;
 int lowvar;
 int b = 0;
 unsigned long lowtime;
-int dataSensor;
+unsigned long averageTime;
+uint16_t sumdata1;
+uint16_t sumdata2;
+int dataSensor1;
+int dataSensor2;
+int sense1;
+int sense2;
+int total = 0;
+int total2 = 0;
+int mdata = 0;
+int ndata = 0;
+int sampleData[10];
+int sampleData2[10];
 uint8_t current = 0;
 uint8_t last = 0;
-uint16_t sense1;
-uint16_t sense2;
-String dataArray3[13];
+String dataArray3[20];
 int r = random(0, 1023);
+//SoftwareSerial mySerial(5, 4);
 
-
-/*convertion ADC value to Binary 12bit*/
-void convert_tobinary(int input){
+void convert_tobinary(int input, int input2){
   dat = 0;
-  for(a=2048; a>=1; a=a/2){ 
+  for(a=512; a>=1; a=a/2){
     if((input-a)>=0){
       dataArray3[dat]= '1';
       input-=a; 
@@ -33,27 +44,37 @@ void convert_tobinary(int input){
     } 
     dat++;  
   }
+    for(a2=512; a2>=1; a2=a2/2){
+    if((input2-a2)>=0){
+      dataArray3[dat]= '1';
+      input2-=a2; 
+    }
+    else{
+      dataArray3[dat]='0';
+    } 
+    dat++;  
+  }
 }
-/*end convertion adc to binary*/
 
-/*Send sensor's data 12bit to optocoupler*/
+
 void send_data(){
+//  mean();
   last = current;
   current = digitalRead(clkIn);
   if(last == 0 && current == 1){
      lowvar = 0;
      digitalWrite(dataOut, dataArray3[b].toInt());
      b++;
-     Serial.print(b);
-     Serial.println("-");
-  }
-  if(b > 13){ //20
+   }
+
+  if(b > 20){
      b = 0;
      lowvar = 1;
      current = 0;
      last = 0;
-     convert_tobinary(dataSensor);
-  }
+     convert_tobinary(dataSensor2, dataSensor1);
+   }
+     
    if(current == LOW){
     if(millis() - lowtime > 1000){ 
      b = 0;
@@ -62,65 +83,80 @@ void send_data(){
    if(current == HIGH){
       lowtime = millis();
    }
+
   else{}
 }
-/*end send data*/
 
-/*Select probe sensor 17Kohm / 2K2ohm*/
-void which_sensor(){
-  sense1 = analogRead(highSensor); //17kohm
-  sense2 = analogRead(lowSensor); // 2,2kohm
-  uint16_t avr_temp;
-  float all_temp;
-  float sendTemp;
-  int convertTemp;
-  if(sense1 >= 1023 && sense2 != 0){
-    // dataSensor = sense2;
-    avr_temp = sense2;
-    all_temp = ((-0.001*(pow(avr_temp, 2))) + (18*avr_temp) + 24125)/1000;
-    sendTemp = all_temp*100;
-    convertTemp = sendTemp;  
-      if(avr_temp <= 20){
-        dataSensor = 0;
-      }
-      else{
-        dataSensor = convertTemp;
-      }
+void mean(){
+ if(millis() - averageTime > 200){  
+  total = total - sampleData2[ndata];
+  sampleData2[ndata] = analogRead(highSensor);
+  total = total + sampleData2[ndata];
+  ndata = ndata + 1;
+  if(ndata >= 10){
+    ndata = 0;
   }
-  else if(sense1 < 1023 && sense2 != 0){
-    // dataSensor = sense1;
-    avr_temp = sense1;
-    all_temp = /*pers 17k ohm*/;
-    sendTemp = all_temp*100;
-    convertTemp = sendTemp;
-      if(avr_temp <= 20){
-        dataSensor = 0;
-      }
-      else{
-        dataSensor = convertTemp;
-      }
+  sumdata2 = total/10;
+
+  total2 = total2 - sampleData[mdata];
+  sampleData[mdata] = analogRead(lowSensor);
+  total2 = total2 + sampleData[mdata];
+  mdata = mdata + 1;
+  if(mdata >= 10){
+    mdata = 0;
   }
-  else if(sense1 == 0 && sense2 == 0){
-    dataSensor = 0;
+  sumdata1 = total2/10;
+
+  if(sumdata1 != 0 && sumdata2 > 1020){
+    dataSensor2 = 0;
+    dataSensor1 = sumdata1;
   }
+  if(sumdata1 == 0 && sumdata2 != 0){
+    dataSensor2 = 1;
+    dataSensor1 = sumdata2;
+  }
+  if(sumdata2 == 0 && sumdata1 == 0){
+    dataSensor2 = 0;
+    dataSensor1 = 0;
+  }
+ }
+
 }
-/*end select probe*/
-
 
 void setup(){
-  analogReference(EXTERNAL);
+  analogReference(INTERNAL4V3);
   Serial.begin(9600);
-  pinMode(dataOut, OUTPUT);
-  pinMode(clkIn, INPUT);
-  pinMode(highSensor, INPUT);
-  pinMode(lowSensor, INPUT);
-  which_sensor();
-  convert_tobinary(dataSensor);
+//  mySerial.begin(9600);
+    pinMode(dataOut, OUTPUT);
+    pinMode(clkIn, INPUT);
+    pinMode(lowSensor, INPUT);
+    pinMode(highSensor, INPUT);
 //    delay(1000);
     b = 0;
 }
 
-void loop(){
-  which_sensor();
+
+void loop(){ 
+//  if(millis() - averageTime < 200){  
+//   sense1 = analogRead(lowSensor);
+//   sense2 = analogRead(highSensor);
+// //  dataSensor1 = sense1;
+// //  dataSensor2 = 1; 
+//   if(sense1 != 0 && sense2 > 1020){
+//     dataSensor2 = 0;
+//     dataSensor1 = sense2;
+//     // dataSensor1 = sumdata1;
+//   }
+//   if(sense1 != 0 && sense2 == 0){
+//     dataSensor2 = 1;
+//     dataSensor1 = sense1;
+//     // dataSensor1 = sumdata2;
+//   }
+//   if(sense1 == 0 && sense2 == 0){
+//     dataSensor2 = 0;
+//     dataSensor1 = 0;
+//   }
+//  }
+  mean();
   send_data();
 }
